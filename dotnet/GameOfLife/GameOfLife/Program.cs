bbeba8e118c02babc80d23e907cafe7023af8fe1
colecathey <cole.cathey@outlook.com>
2015-10-05 20:19:37 -0500

    updates

--- dotnet/GameOfLife/GameOfLife/Program.cs
+++ dotnet/GameOfLife/GameOfLife/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class Life
+    {
+        private bool[,] world;
+        private bool[,] nextGeneration;
+        private Task processTask;
+
+        public int Size { get; set; }
+        public int Generation { get; set; }
+
+        public void WorldSize(int size)
+        {
+            if(size < 1) throw new ArgumentException("Size must be 1 or larger");
+            this.Size = size;
+            world = new bool[size, size];
+            nextGeneration = new bool[size, size];            
+        }
+
+        public bool this[int x, int y]
+        {
+            get { return this.world[x, y]; }
+            set { this.world[x, y] = value; }
+        }
+
+        public Action<bool[,]> NextGenerationCompleted;
+
+        public bool Cell(int x,int y)
+        {
+            bool currentValue = this.world[x, y];
+            return this.world[x, y] = !currentValue;
         }
     }
 }
